00f41e7ce7ed61af7bee85bf93d65d13a5b26946
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-04-29 05:56:23 +0600

    add steps

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,6 +16,21 @@ async function run(){
    try{
        await client.connect();
        const serviceCollection = client.db('geniusCar').collection('service');
+       const orderCollection = client.db('geniusCar').collection('order');
+
+
+         //Auth
+
+      app.post('/login', async(req, res) =>{
+        const user = req.body;
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn: 'id'
+        });
+        res.send({accessToken});
+    })
+
+    //services api
+
       app.get('/service', async(req, res) => {
         const query = {};
         const cursor = serviceCollection.find(query);
@@ -39,6 +55,25 @@ async function run(){
           const id = req.params.id;
           const query = {_id: ObjectId(id)};
           const result = await serviceCollection.deleteOne(query);
+          res.send(result);
+      })
+
+    
+
+      //order collcetion api
+
+      app.get('/order', async(req,res) =>{
+          const email = req.query.email
+          const query =  {email: email};
+          console.log(query);
+          const cursor = orderCollection.find(query);
+          const orders = await cursor.toArray();
+          res.send(orders);
+      })
+
+      app.post('/order', async(req, res) =>{
+          const order = req.body;
+          const result = await orderCollection.insertOne(order);
           res.send(result);
       })
 
